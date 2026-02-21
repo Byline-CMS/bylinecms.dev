a6d6c1580221f100930cbdfcd8529cc87f4e4241
Anthony Bouch <anthony@infonomic.io>
2026-02-22 01:39:51 +0700

    docs: LICENSE header update

--- notice.js
+++ notice.js
@@ -13,7 +13,7 @@ const fileExtensions = ['.js', '.jsx', '.ts', '.tsx']
 
 // The copyright notice to add
 const copyrightNotice = `/**
- * This Source Code Form is subject to the terms of the Mozilla Public
+ * This Source Code is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * 
