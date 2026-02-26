878aea6775d90c7fdbda1b556be6027664a1e3f8
Anthony Bouch <anthony@infonomic.io>
2026-02-26 08:40:39 +0700

    docs: Updated copyright notices

--- apps/webapp/src/ui/fields/richtext/richtext-lexical/field/types.ts
+++ apps/webapp/src/ui/fields/richtext/richtext-lexical/field/types.ts
@@ -1,3 +1,11 @@
+/**
+ * This Source Code is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) Infonomic Company Limited
+ */
+
 import type { EditorState, LexicalEditor } from 'lexical'
 
 import type { EditorSettings } from './config/types'
