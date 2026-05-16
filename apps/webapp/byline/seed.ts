0657b966fad15c6722c33b881256394383703861
Anthony Bouch <anthony@infonomic.io>
2026-05-16 20:18:35 +0700

    feat(webapp): removed docs-categories seed

--- apps/webapp/byline/seed.ts
+++ apps/webapp/byline/seed.ts
@@ -11,13 +11,11 @@ import 'dotenv/config'
 import './server.config.js'
 
 import { seedAdmin } from './seeds/admin.js'
-import { seedDocsCategories } from './seeds/doc-categories.js'
 import { seedDocs } from './seeds/docs.js'
 import { seedNewsCategories } from './seeds/news-categories.js'
 
 async function run() {
   await seedAdmin()
-  await seedDocsCategories()
   await seedNewsCategories()
   await seedDocs()
 }
